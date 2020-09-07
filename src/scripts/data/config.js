e0a64acb65033209206680dcc17d1ab65e1c1db3
dazelpro <dazelpro@gmail.com>
2020-09-07 14:53:48 +0700

    Database local PWA 1.1

--- src/scripts/data/config.js
+++ src/scripts/data/config.js
@@ -4,6 +4,9 @@ const CONFIG = {
     BASE_IMAGE_URL_MEDIUM: 'https://dicoding-restaurant-api.el.r.appspot.com/images/medium/',
     BASE_IMAGE_URL_SMALL: 'https://dicoding-restaurant-api.el.r.appspot.com/images/small/',
     DEFAULT_LANGUAGE: 'en-us',
+    DATABASE_NAME: 'laper-apps',
+    DATABASE_VERSION: 1,
+    OBJECT_STORE_NAME: 'favorite',
 };
    
 export default CONFIG;
