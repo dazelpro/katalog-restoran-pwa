c6a5f0d00ac992eb15571c261b72eae2726bcf65
dazelpro <dazelpro@gmail.com>
2020-09-07 11:39:54 +0700

    detail component

--- src/scripts/pages/detail.js
+++ src/scripts/pages/detail.js
@@ -17,24 +17,50 @@ const Detail = {
     async afterRender() {
         const url = UrlParser.parseActiveUrlWithoutCombiner();
         let dataDetail = '';
+        let listCategory = '';
+        let listMakanan = '';
+        let listMinuman = '';
         const data = await sourceData.detailResto(url.id);
         console.log(data)
+        data['restaurant']['categories'].forEach(function(data) {
+            listCategory +=`
+                <div class="tag">${data['name']}</div>
+            `;
+        });
+        data['restaurant']['menus']['foods'].forEach(function(data) {
+            listMakanan +=`
+                ${data['name']},
+            `;
+        });
+        data['restaurant']['menus']['drinks'].forEach(function(data) {
+            listMinuman +=`
+                ${data['name']},
+            `;
+        });
         dataDetail += `
             <div class="list_item">
                 <img class="list_item_img" src="${CONFIG.BASE_IMAGE_URL_MEDIUM+data['restaurant']['pictureId']}" alt="${data['restaurant']['name']}" title="${data['restaurant']['name']}">
                 <div class="city">${data['restaurant']['city']}</div>
-                <div class="list_item_content">
+                <div class="list_item_content" style="text-align:left;">
                     <p class="list_item_rating">
                         Rating : 
                         <a href="#" class="list_item_rating_value">${data['restaurant']['rating']}</a>
                     </p>
-                    <h1 class="list_item_title"><a href="/#/detail/${data['restaurant']['id']}">${data['restaurant']['name']}</a></h1>
-                    <div class="list_item_desc">${data['restaurant']['description'].slice(0, 150)}...</div>
+                    <h2>${data['restaurant']['name']}</h2>
+                    <p class="alamat">${data['restaurant']['address']}</p>
+                    <div class="list_item_desc_detail">${data['restaurant']['description']}</div>
+                    <br>
+                    <h2>Categori Menu</h2>
+                    <div style="margin-top:10px;margin-bottom:20px">${listCategory}</div>
+                    <h3>Makanan</h3>
+                    <div style="margin-top:10px;margin-bottom:20px">${listMakanan}</div>
+                    <h3>Minuman</h3>
+                    <div style="margin-top:10px;margin-bottom:20px">${listMinuman}</div>
                 </div>
             </div>
         `;
-        document.querySelector('#restoName').innerHTML = `${data['restaurant']['name']}`;
-        document.querySelector('#detail').innerHTML = dataDetail;  
+        document.querySelector('#restoName').innerHTML = `DETAIL RESTORAN`;
+        document.querySelector('#detail').innerHTML = dataDetail;
     },
 };
    
