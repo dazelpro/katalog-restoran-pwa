ca8cdf902e49031002ac61e21ad7d88f114d58e8
dazelpro <dazelpro@gmail.com>
2020-09-07 14:51:28 +0700

    PWA 1.2

---
+++ src/scripts/cache-helper.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = new Date().toISOString();
+const CacheHelper = {
+    async cachingAppShell(requests) {
+        const cache = await this._openCache();
+        cache.addAll(requests);
+    },
+    
+    async deleteOldCache() {
+        const cacheNames = await caches.keys();
+        cacheNames
+        .filter((name) => name !== CACHE_NAME)
+        .map((filteredName) => caches.delete(filteredName));
+    },
+    
+    async revalidateCache(request) {
+        const response = await caches.match(request);
+    
+        if (response) {
+        return response;
+        }
+        return this._fetchRequest(request);
+    },
+    
+    async _openCache() {
+        return caches.open(CACHE_NAME);
+    },
+    
+    async _fetchRequest(request) {
+        const response = await fetch(request);
+    
+        if (!response || response.status !== 200) {
+        return response;
+        }
+    
+        await this._addCache(request);
+        return response;
+    },
+    
+    async _addCache(request) {
+        const cache = await this._openCache();
+        cache.add(request);
+    },
+};
+ 
+export default CacheHelper;
--- src/scripts/sw.js
+++ src/scripts/sw.js
@@ -1,12 +1,16 @@
+import 'regenerator-runtime';
+import CacheHelper from './cache-helper';
+
+const { assets } = global.serviceWorkerOption;
+
 self.addEventListener('install', (event) => {
-    console.log('Installing Service Worker ...');
+    event.waitUntil(CacheHelper.cachingAppShell([...assets, './']));
 });
    
 self.addEventListener('activate', (event) => {
-    console.log('Activating Service Worker ...');
+    event.waitUntil(CacheHelper.deleteOldCache());
 });
    
 self.addEventListener('fetch', (event) => {
-    console.log(event.request);
-    event.respondWith(fetch(event.request));
+    event.respondWith(CacheHelper.revalidateCache(event.request));
 });
