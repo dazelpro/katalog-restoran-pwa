78f4951d7d98fdcd881220df020761c052c2c9ec
dazelpro <dazelpro@gmail.com>
2020-09-08 13:06:22 +0700

    Database Local PWA 1.5

--- src/scripts/pages/favorite.js
+++ src/scripts/pages/favorite.js
@@ -1,4 +1,4 @@
-import sourceData from '../data/source';
+import FavoriteIdb from '../data/database-idb';
 import CONFIG from '../data/config';
 
 const Favorite = {
@@ -6,7 +6,7 @@ const Favorite = {
         return `
         <section class="content">
             <div class="latest">
-                <h1>Explore Restaurant</h1>
+                <h1>Explore Favorite Restaurant</h1>
                 <div class="list" id="tes"></div>
             </div>
         </section>
@@ -14,9 +14,29 @@ const Favorite = {
     },
    
     async afterRender() {
-        const resto = await sourceData.listResto();
+        // const resto = await sourceData.listResto();
+        // let dataList = '';
+        // resto['restaurants'].forEach(function(data) {
+        //     dataList +=`
+        //     <div class="list_item">
+        //         <img class="list_item_thumb" src="${CONFIG.BASE_IMAGE_URL_SMALL+data['pictureId']}" alt="${data['name']}" title="${data['name']}">
+        //         <div class="city">${data['city']}</div>
+        //         <div class="list_item_content">
+        //             <p class="list_item_rating">
+        //                 Rating : 
+        //                 <a href="#" class="list_item_rating_value">${data['rating']}</a>
+        //             </p>
+        //             <h1 class="list_item_title"><a href="/#/detail/${data['id']}">${data['name']}</a></h1>
+        //             <div class="list_item_desc">${data['description'].slice(0, 150)}...</div>
+        //         </div>
+        //     </div>
+        //     `;
+        // });
+        // document.querySelector('#tes').innerHTML = dataList;
+        const resto = await FavoriteIdb.getAllFavorite();
+        console.log(resto)
         let dataList = '';
-        resto['restaurants'].forEach(function(data) {
+        resto.forEach(function(data) {
             dataList +=`
             <div class="list_item">
                 <img class="list_item_thumb" src="${CONFIG.BASE_IMAGE_URL_SMALL+data['pictureId']}" alt="${data['name']}" title="${data['name']}">
@@ -32,7 +52,7 @@ const Favorite = {
             </div>
             `;
         });
-        document.querySelector('#tes').innerHTML = dataList;  
+        document.querySelector('#tes').innerHTML = dataList;
     },
 };
    
