8b7befa4182093c4edce31a5d22c7acd033903a6
dazelpro <dazelpro@gmail.com>
2020-09-14 13:58:48 +0700

    e2e testing

--- e2e/Liking_Movies.spec.js
+++ e2e/Liking_Movies.spec.js
@@ -1,5 +1,10 @@
 Feature('Liking Movies');
 
-Scenario('test something', (I) => {
+Before((I) => {
+    I.amOnPage('/#/favorite');
+});
 
+Scenario('showing empty liked movies', (I) => {
+    I.seeElement('#tes');
+    I.see('Tidak ada film untuk ditampilkan', '.movie-item__not__found');
 });
--- src/scripts/pages/favorite.js
+++ src/scripts/pages/favorite.js
@@ -8,6 +8,7 @@ const Favorite = {
             <div class="latest">
                 <h1>Explore Favorite Restaurant</h1>
                 <div class="list" id="tes"></div>
+                <div class="movie-item__not__found"></div>
             </div>
         </section>
         `;
@@ -16,23 +17,27 @@ const Favorite = {
     async afterRender() {
         const resto = await FavoriteIdb.getAllFavorite();
         let dataList = '';
-        resto.forEach((data) => {
-            dataList += `
-            <div class="list_item">
-                <img class="list_item_thumb" src="${CONFIG.BASE_IMAGE_URL_SMALL + data.pictureId}" alt="${data.name}" title="${data.name}">
-                <div class="city">${data.city}</div>
-                <div class="list_item_content">
-                    <p class="list_item_rating">
-                        Rating : 
-                        <a href="#" class="list_item_rating_value">${data.rating}</a>
-                    </p>
-                    <h1 class="list_item_title"><a href="/#/detail/${data.id}">${data.name}</a></h1>
-                    <div class="list_item_desc">${data.description.slice(0, 150)}...</div>
+        if(resto.length === 0){
+            document.querySelector('.movie-item__not__found').innerHTML = 'Tidak ada film untuk ditampilkan';
+        }else{
+            resto.forEach((data) => {
+                dataList += `
+                <div class="list_item">
+                    <img class="list_item_thumb" src="${CONFIG.BASE_IMAGE_URL_SMALL + data.pictureId}" alt="${data.name}" title="${data.name}">
+                    <div class="city">${data.city}</div>
+                    <div class="list_item_content">
+                        <p class="list_item_rating">
+                            Rating : 
+                            <a href="#" class="list_item_rating_value">${data.rating}</a>
+                        </p>
+                        <h1 class="list_item_title"><a href="/#/detail/${data.id}">${data.name}</a></h1>
+                        <div class="list_item_desc">${data.description.slice(0, 150)}...</div>
+                    </div>
                 </div>
-            </div>
-            `;
-        });
-        document.querySelector('#tes').innerHTML = dataList;
+                `;
+            })
+            document.querySelector('#tes').innerHTML = dataList;
+        }
     },
 };
 
